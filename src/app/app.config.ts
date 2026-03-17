2c81fe8a3169ff8dd568a61d9a95954ea17b6938
Joinsider <johannes@fampopprol.me>
2026-03-17 14:20:29 +0100

    Add swagger autogeneration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, Component, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+      theme: {
+        preset: Aura, // or Material, Lara, Nora
+      },
+    }),
     provideRouter(routes)
-  ]
+  ],
 };
