9ef7d32002a7192695b8f73655c70ef9ac683031
Joinsider <johannes@fampopprol.me>
2026-03-17 18:04:52 +0100

    Add environment configuration and update app setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,37 @@
-import { ApplicationConfig, Component, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  Component,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
+import { ApiModule, Configuration } from './api';
+import { environment } from '../environments/environment';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     providePrimeNG({
       theme: {
         preset: Aura, // or Material, Lara, Nora
       },
     }),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(
+      ApiModule.forRoot(
+        () =>
+          new Configuration({
+            basePath: environment.apiBasePath,
+            credentials: {
+              bearerAuth: () => localStorage.getItem('accessToken') ?? '',
+            },
+          }),
+      ),
+    ),
   ],
 };
