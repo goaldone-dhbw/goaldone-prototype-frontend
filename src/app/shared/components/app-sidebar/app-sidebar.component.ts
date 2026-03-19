01c2be5770b506500a8c785b54e0c566e618238c
Joinsider <johannes@fampopprol.me>
2026-03-19 14:20:11 +0100

    Update sidebar to include settings

--- src/app/shared/components/app-sidebar/app-sidebar.component.ts
+++ src/app/shared/components/app-sidebar/app-sidebar.component.ts
@@ -1,22 +1,28 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Menu } from 'primeng/menu';
+import { Image } from 'primeng/image';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [Menu],
-  templateUrl: './app-sidebar.component.html',
-  styleUrl: './app-sidebar.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [Menu, Image, RouterLink],
+    templateUrl: './app-sidebar.component.html',
+    styleUrl: './app-sidebar.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppSidebarComponent {
-  protected readonly navigationItems: MenuItem[] = [
-    {
-      label: 'Workspace',
-      icon: 'pi pi-home',
-      routerLink: '/app',
-    },
-  ];
+    protected readonly navigationItems: MenuItem[] = [
+        {
+            label: 'Start',
+            icon: 'pi pi-home',
+            routerLink: '/app',
+        },
+        {
+            label: 'Einstellungen',
+            icon: 'pi pi-cog',
+            routerLink: '/app/settings',
+        },
+    ];
 }
-
