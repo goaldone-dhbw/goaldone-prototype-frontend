e0ab1770c2a688272834f414ebdabcfa98af0f90
LeonProgrammiert <leon.freudenmann@gmail.com>
2026-03-20 13:36:19 +0100

    Initial branch upload

--- src/app/features/schedule/schedule.page.ts
+++ src/app/features/schedule/schedule.page.ts
@@ -1,14 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Card } from 'primeng/card';
+import { Button } from 'primeng/button';
+
 
 @Component({
   selector: 'app-schedule-page',
   standalone: true,
-  imports: [Card],
+  imports: [Card, Button],
   templateUrl: './schedule.page.html',
   styleUrl: './schedule.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SchedulePage {
   protected readonly title = 'Planungsansicht';
+  protected button = {
+    label: 'Aufgabe hinzufügen',
+    icon: 'pi pi-plus',
+    class: 'p-button-success',
+  };
+
+  onStartPlanning() {
+
+  }
 }
