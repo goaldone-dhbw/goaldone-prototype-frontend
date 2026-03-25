93b23c802ff39f8ae8dd1938570356b76498e94b
Leon Freudenmann <124998500+LeonProgrammiert@users.noreply.github.com>
2026-03-25 15:06:03 +0100

    Feature/schedule page (#13)
    * Initial branch upload
    
    * Form via button click
    
    * Added AddTaskDialog and Calendar-View
    
    * Updated dependencies
    
    * Updated schedule page

--- src/app/features/schedule/schedule.page.ts
+++ src/app/features/schedule/schedule.page.ts
@@ -1,14 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Card } from 'primeng/card';
+import {ChangeDetectionStrategy, Component, ViewChild} from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+import { CalendarComponent } from './calendar/caelndar.component'
+import { AddTaskDialog } from '../../shared/components/add-task-dialog/add-task-dialog.component';
 
 @Component({
   selector: 'app-schedule-page',
   standalone: true,
-  imports: [Card],
+  imports: [CalendarComponent, CardModule, ButtonModule, AddTaskDialog],
   templateUrl: './schedule.page.html',
   styleUrl: './schedule.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class SchedulePage {
+  @ViewChild(AddTaskDialog) addTaskDialog!: AddTaskDialog;
+
   protected readonly title = 'Planungsansicht';
+
+  protected addTaskButton = {
+    label: 'Aufgabe hinzufügen',
+    icon: 'pi pi-plus',
+    severity: 'success',
+  };
+
+  protected scheduleTasksButton = {
+    label: 'Planung starten',
+    icon: 'pi pi-calendar',
+    severity: 'primary',
+  }
+
+
+  onAddTask() {
+    console.log('Dialog geöffnet');
+    this.addTaskDialog.openDialog()
+  }
+
+  scheduleTasks() {
+    console.log('Scheduling tasks')
+  }
 }
