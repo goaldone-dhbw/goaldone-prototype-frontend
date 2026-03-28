12fc5ddb2fc1a344e907f8c1e2f101b62c511d3c
theagehring <thea.gehring@icloud.com>
2026-03-28 09:58:01 +0100

    optimized and styled landingpage

--- src/app/features/landing/landing.page.ts
+++ src/app/features/landing/landing.page.ts
@@ -2,12 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Button } from 'primeng/button';
 import { Card } from 'primeng/card';
-import { PrimeTemplate } from 'primeng/api';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [Button, Card, RouterLink, PrimeTemplate],
+  imports: [Button, Card, RouterLink],
   templateUrl: './landing.page.html',
   styleUrl: './landing.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
